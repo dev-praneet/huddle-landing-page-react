033349da099e8f443ee2d87a2e1a3549f2ba378b
Praneet Singh <Praneet@PRANEETs-MacBook-Air.local>
2021-12-02 20:26:47 +0530

    added mobile version of Content section

---
+++ src/components/Content.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { Dummy } from './Dummy';
+
+
+const Container = styled.div`
+    h1 {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+        color: white;
+        text-align: center;
+    }
+
+    p {
+        font-family: 'Open Sans', sans-serif;
+        font-weight: 400;
+        color: hsl(0deg, 0%, 80%);
+        text-align: center;
+        font-size: 1.3rem;
+        line-height: 1.5;
+    }
+`;
+
+const Button = styled.button`
+    display: block;
+    margin: 0 auto;
+    padding: 1em 6em;
+    border: none;
+    border-radius: 2em;
+    font-size: 1.1rem;
+    color: var(--purple);
+`;
+
+const Content: React.FC = () => {
+    return (
+        <Container>
+            <h1>
+                Build The Community Your Fans Will Love
+            </h1>
+            <Dummy height='1.5'/>
+            <p>
+                Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.
+            </p>
+            <Dummy height='2'/>
+            <Button>
+                Register
+            </Button>
+        </Container>
+    )
+} 
+
+export default Content;

b5f16bab59aa05f41acc4f75040a3ec7614565ff
Praneet Singh <Praneet@PRANEETs-MacBook-Air.local>
2021-12-03 16:04:15 +0530

    added the individual components for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import { Dummy } from './components/Dummy';
+import { GlobalStyle } from './GlobalStyle';
+
+import bgMobile from './images/bg-mobile.svg';
+import bgDesktop from './images/bg-desktop.svg';
+
+
+const Wrapper = styled.div`
+  background: var(--purple);
+`;
+
+const FirstHalf = styled.div`
+  background: url(${bgMobile}) no-repeat;
+  background-position: center;
+  background-size: cover; 
+  `;
+  
+  const SecondHalf = styled.div`
+  
+  `;
+  
+  const Container = styled.div`
+  max-width: 80%;
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <GlobalStyle/>
+      <FirstHalf>
+        <Container>
+          <Dummy height='2'/>
+          <Navigation />
+          <Dummy height='3.5'/>
+          <Hero />
+          <Dummy height='2'/>
+        </Container>
+      </FirstHalf>
+      <SecondHalf>
+        <Container>
+          <Dummy height='2'/>
+          <Content/>
+          <Dummy height='5'/>
+          <Footer/>
+          <Dummy height='3'/>
+        </Container>
+      </SecondHalf>
+    </Wrapper>
   );
 }
 
